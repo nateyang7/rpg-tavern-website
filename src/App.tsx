396e485e6ca9608b75867341cd666836a325026e
Nathan Yang <nathanyang.w@outlook.com>
2026-05-25 07:40:22 +0200

    Add About component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import NavBar from "./components/NavBar.tsx";
 import Home from "./components/Home.tsx";
 import Menu from "./components/Menu.tsx";
+import About from "./components/About.tsx";
 import Footer from "./components/Footer.tsx";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Home />
         <Menu />
+        <About />
       </main>
 
       <Footer />
---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+// src/components/About.tsx
+
+export default function About() {
+  return (
+    <section id="about">
+      <h2>About us</h2>
+      <p>We are blabla from blabla. Happy to serve new adventures!</p>
+    </section>
+  );
+}
