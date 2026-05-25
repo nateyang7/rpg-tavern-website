84c60f2f295f5be271db721ca11293d47d20b334
Nathan Yang <nathanyang.w@outlook.com>
2026-05-25 07:29:12 +0200

    Add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import NavBar from "./components/NavBar.tsx";
+import Home from "./components/Home.tsx";
 
 function App() {
   return (
     <>
       <NavBar />
+
+      <main>
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+// src/components/Home.tsx
+
+export default function Home() {
+  return (
+    <section id="home">
+      <h2>Welcome to the Tavern</h2>
+    </section>
+  );
+}
