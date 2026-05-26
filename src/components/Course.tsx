fa4792570e5bcc8e62bdf1b667642e4d27a22453
Nathan Yang <nathanyang.w@outlook.com>
2026-05-26 15:31:15 +0200

    Update Course component with gold notation

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -2,6 +2,7 @@
 
 import type { Item } from "../data/item.ts";
 
+// TODO: Replace GOLD by a coin icon
 export default function Course(item: Item) {
   return (
     <div id={item.name}>
@@ -10,7 +11,7 @@ export default function Course(item: Item) {
         <strong>{item.name}</strong>
       </h4>
       <p>{item.description}</p>
-      <p>{item.price}</p>
+      <p>{item.price} G</p>
     </div>
   );
 }
